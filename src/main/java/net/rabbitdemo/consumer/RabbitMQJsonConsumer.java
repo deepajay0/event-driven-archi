649f4c45957ebb8c5c76296dc3b3d50f20bf64d4
ajay deep <ajay012deep@gmail.com>
2024-02-11 22:51:36 +0530

    first commit

---
+++ src/main/java/net/rabbitdemo/consumer/RabbitMQJsonConsumer.java
@@ -0,0 +1,19 @@
+package net.rabbitdemo.consumer;
+
+import net.rabbitdemo.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQJsonConsumer {
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQJsonConsumer.class);
+
+    @RabbitListener(queues = {"${rabbitmq.queue.json.name}"})
+    public void consumeJsonMessages(User user){
+        logger.info(String.format("Received JSON messages -> %s",user));
+        
+
+    }
+}
